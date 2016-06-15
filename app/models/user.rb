7368fce087e3a8caa2e2243e25d7ace7fd70db3e
Nayan Gupta <nayan.affimintus@gmail.com>
2016-06-15 18:06:09 +0530

    some changes done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatableend
+
+end 
