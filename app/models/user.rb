20df9a4fa8d2954998e2159aedd3c501b14d044c
Nayan Gupta <nayan.affimintus@gmail.com>
2016-06-15 19:26:29 +0530

    some changes done in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,24 +9,24 @@ class User < ActiveRecord::Base
   before_create :prepare_username,:gen_password
   after_create :send_mail
 
-      def prepare_username
-        self.username = (self.firstname[0..2] +"."+self.lastname).to_s.downcase
-      end
+	def prepare_username
+		self.username = (self.firstname[0..2] +"."+self.lastname).to_s.downcase
+	end
 
-      def password_required?
- 			false
- 	  end
- 
- 	  def email_required?
-  			true
- 	  end
+	def password_required?
+		false
+	end
 
- 	  def gen_password
- 		 o =  [('a'..'z'), (0..9)].map{|i| i.to_a}.flatten
-  	     self.password = self.password_confirmation = self.pwd = (0..5).map{ o[rand(o.length)] }.join if self.password.blank?
-  	  end
+	def email_required?
+	 true
+	end
 
-      def send_mail
-  	   UserMailer.welcome(self).deliver
-      end
+	def gen_password
+		 rand_password =  [('a'..'z'), (0..9)].map{|i| i.to_a}.flatten
+		self.password = self.password_confirmation = self.pwd = (0..5).map{ rand_password[rand(rand_password.length)] }.join if self.password.blank?
+	end
+
+	def send_mail
+	UserMailer.welcome(self).deliver
+	end
 end
