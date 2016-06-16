50e258b5b04334a028378edfad076ce76a28aa5c
purvakaranjawala <purva.affimintus@gmail.com>
2016-06-16 12:56:15 +0530

    User and employee controller generated

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,5 +4,11 @@ class EmployeesController < ApplicationController
 	end
 	
 	def index
+		@employees =EmployeeDetail.all
 	end
+
+	private
+		def employee_params
+      params.require(:employee_detail).permit(:user_id, :user_name,:first_name,:last_name,:dob,:doj,:blood_group,:designation,:oficial_email_id,:mobile,:landline,:current_address,:permanent_address,:skype_id,:personal_email_id,:guardian_full_name,:guardian_mobile,:guardian_landline,:guardian_address,:avatar)
+    end
 end
